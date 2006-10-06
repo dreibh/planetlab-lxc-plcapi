21d36f891935022da612b2cc6e5a9ddbca7c2cd1
Mark Huang <mlhuang@cs.princeton.edu>
2006-10-06 19:02:33 +0000

    - implement interface to Addresses

--- PLC/Addresses.py
+++ PLC/Addresses.py
@@ -1,6 +1,7 @@
 from PLC.Faults import *
 from PLC.Parameter import Parameter
 from PLC.Table import Row, Table
+from PLC.AddressTypes import AddressType, AddressTypes
 
 class Address(Row):
     """
@@ -12,23 +13,88 @@ class Address(Row):
     primary_key = 'address_id'
     fields = {
         'address_id': Parameter(int, "Address identifier"),
-        'line1': Parameter(str, "Address line 1"),
-        'line2': Parameter(str, "Address line 2"),
-        'line3': Parameter(str, "Address line 3"),
-        'city': Parameter(str, "City"),
-        'state': Parameter(str, "State or province"),
-        'postalcode': Parameter(str, "Postal code"),
-        'country': Parameter(str, "Country"),
-        'address_type': Parameter(str, "Address type"),
+        'line1': Parameter(str, "Address line 1", max = 254, optional = False),
+        'line2': Parameter(str, "Address line 2", max = 254),
+        'line3': Parameter(str, "Address line 3", max = 254),
+        'city': Parameter(str, "City", max = 254, optional = False),
+        'state': Parameter(str, "State or province", max = 254, optional = False),
+        'postalcode': Parameter(str, "Postal code", max = 64, optional = False),
+        'country': Parameter(str, "Country", max = 128, optional = False),
+        'site_id': Parameter(str, "Site identifier"),
+        'address_type_ids': Parameter([int], "Address type identifiers", ro = True),
+        'address_types': Parameter([str], "Address types", ro = True),
         }
 
     def __init__(self, api, fields = {}):
-        self.api = api
         Row.__init__(self, fields)
+        self.api = api
+
+    def add_address_type(self, address_type, commit = True):
+        """
+        Add address type to existing address.
+        """
+
+        assert 'address_id' in self
+        assert isinstance(address_type, AddressType)
+        assert 'address_type_id' in address_type
+
+        address_id = self['address_id']
+        address_type_id = address_type['address_type_id']
+
+        if address_type_id not in self['address_type_ids']:
+            assert address_type['name'] not in self['address_types']
+
+            self.api.db.do("INSERT INTO address_address_type (address_id, address_type_id)" \
+                           " VALUES(%(address_id)d, %(address_type_id)d)",
+                           locals())
+
+            if commit:
+                self.api.db.commit()
+
+            self['address_type_ids'].append(address_type_id)
+            self['address_types'].append(address_type['name'])
+
+    def remove_address_type(self, address_type, commit = True):
+        """
+        Add address type to existing address.
+        """
+
+        assert 'address_id' in self
+        assert isinstance(address_type, AddressType)
+        assert 'address_type_id' in address_type
+
+        address_id = self['address_id']
+        address_type_id = address_type['address_type_id']
+
+        if address_type_id in self['address_type_ids']:
+            assert address_type['name'] in self['address_types']
+
+            self.api.db.do("DELETE FROM address_address_type" \
+                           " WHERE address_id = %(address_id)d" \
+                           " AND address_type_id = %(address_type_id)d",
+                           locals())
+
+            if commit:
+                self.api.db.commit()
+
+            self['address_type_ids'].remove(address_type_id)
+            self['address_types'].remove(address_type['name'])
 
     def delete(self, commit = True):
-        # XXX
-        pass
+        """
+        Delete existing address from the database.
+        """
+
+        assert 'address_id' in self
+
+        # Clean up miscellaneous join tables
+        for table in ['address_address_type', 'addresses']:
+            self.api.db.do("DELETE FROM %s" \
+                           " WHERE address_id = %d" % \
+                           (table, self['address_id']), self)
+
+        if commit:
+            self.api.db.commit()
 
 class Addresses(Table):
     """
@@ -37,5 +103,24 @@ class Addresses(Table):
     """
 
     def __init__(self, api, address_id_list = None):
-        # XXX
-        pass
+	self.api = api
+
+        sql = "SELECT %s FROM view_addresses" % \
+              ", ".join(Address.fields)
+
+        if address_id_list:
+            sql += " WHERE address_id IN (%s)" % ", ".join(map(str, address_id_list))
+
+        rows = self.api.db.selectall(sql)
+
+        for row in rows:
+            self[row['address_id']] = address = Address(api, row)
+            for aggregate in 'address_type_ids', 'address_types':
+                if not address.has_key(aggregate) or address[aggregate] is None:
+                    address[aggregate] = []
+                else:
+                    elements = address[aggregate].split(',')
+                    try:
+                        address[aggregate] = map(int, elements)
+                    except ValueError:
+                        address[aggregate] = elements
