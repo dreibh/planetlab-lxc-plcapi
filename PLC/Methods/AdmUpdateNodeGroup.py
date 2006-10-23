b2c4eb4272360a91486635d0b7c4ce241995861c
Mark Huang <mlhuang@cs.princeton.edu>
2006-10-23 20:37:26 +0000

    - fix UpdateNodeGroup calling convention

--- PLC/Methods/AdmUpdateNodeGroup.py
+++ PLC/Methods/AdmUpdateNodeGroup.py
@@ -1,3 +1,8 @@
+from PLC.Faults import *
+from PLC.Method import Method
+from PLC.Parameter import Parameter, Mixed
+from PLC.NodeGroups import NodeGroup, NodeGroups
+from PLC.Auth import PasswordAuth
 from PLC.Methods.UpdateNodeGroup import UpdateNodeGroup
 
 class AdmUpdateNodeGroup(UpdateNodeGroup):
@@ -6,3 +11,17 @@ class AdmUpdateNodeGroup(UpdateNodeGroup):
     """
 
     status = "deprecated"
+
+    accepts = [
+        PasswordAuth(),
+        Mixed(NodeGroup.fields['nodegroup_id'],
+	      NodeGroup.fields['name']),
+        NodeGroup.fields['name'],
+     	NodeGroup.fields['description']
+        ]
+
+    returns = Parameter(int, '1 if successful')
+
+    def call(self, auth, nodegroup_id_or_name, name, description):
+        return UpdateNodeGroup.call(self, auth, nodegroup_id_or_name,
+                                    {'name': name, 'description': description})
