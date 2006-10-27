ae114f828e181c0e697150413700bfe34102398d
Mark Huang <mlhuang@cs.princeton.edu>
2006-10-27 20:08:46 +0000

    - install schema

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2006 The Trustees of Princeton University
 #
-# $Id: setup.py,v 1.2 2006/10/27 19:53:06 mlhuang Exp $
+# $Id: setup.py,v 1.3 2006/10/27 19:56:55 mlhuang Exp $
 #
 
 from distutils.core import setup
@@ -13,4 +13,5 @@ from distutils.core import setup
 setup(py_modules = ['ModPython'],
       packages = ['PLC', 'PLC/Methods', 'PLC/Methods/system'],
       scripts = ['Shell.py', 'Server.py', 'Test.py'],
-      data_files = [('php', ['php/plc_api.php'])])
+      data_files = [('', ['planetlab4.sql']),
+                    ('php', ['php/plc_api.php'])])
