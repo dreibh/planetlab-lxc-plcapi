b1665467eef1cd6b6e0defa29ee59f1f2c4e694e
Mark Huang <mlhuang@cs.princeton.edu>
2006-10-27 19:56:55 +0000

    - include system. methods

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2006 The Trustees of Princeton University
 #
-# $Id: setup.py,v 1.1 2006/10/27 18:54:21 mlhuang Exp $
+# $Id: setup.py,v 1.2 2006/10/27 19:53:06 mlhuang Exp $
 #
 
 from distutils.core import setup
 
 setup(py_modules = ['ModPython'],
-      packages = ['PLC', 'PLC/Methods'],
+      packages = ['PLC', 'PLC/Methods', 'PLC/Methods/system'],
       scripts = ['Shell.py', 'Server.py', 'Test.py'],
       data_files = [('php', ['php/plc_api.php'])])
