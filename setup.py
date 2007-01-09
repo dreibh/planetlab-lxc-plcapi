e16ed42a4b13481416501588412e5b6e3aa4135f
Mark Huang <mlhuang@cs.princeton.edu>
2007-01-09 16:33:16 +0000

    - install plcsh instead of Shell.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2006 The Trustees of Princeton University
 #
-# $Id: setup.py,v 1.3 2006/10/27 19:56:55 mlhuang Exp $
+# $Id: setup.py,v 1.4 2006/10/27 20:08:46 mlhuang Exp $
 #
 
 from distutils.core import setup
 
 setup(py_modules = ['ModPython'],
       packages = ['PLC', 'PLC/Methods', 'PLC/Methods/system'],
-      scripts = ['Shell.py', 'Server.py', 'Test.py'],
+      scripts = ['plcsh', 'Server.py', 'Test.py'],
       data_files = [('', ['planetlab4.sql']),
                     ('php', ['php/plc_api.php'])])
