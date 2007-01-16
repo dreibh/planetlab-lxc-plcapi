34e1a55391f712d24f3932b98c7f26fcbd84baea
Mark Huang <mlhuang@cs.princeton.edu>
2007-01-16 17:08:53 +0000

    - accept message_filter and return_fields

--- PLC/Methods/GetMessages.py
+++ PLC/Methods/GetMessages.py
@@ -1,25 +1,31 @@
 from PLC.Faults import *
 from PLC.Method import Method
-from PLC.Parameter import Parameter
+from PLC.Parameter import Parameter, Mixed
+from PLC.Filter import Filter
 from PLC.Messages import Message, Messages
 from PLC.Auth import Auth
 
 class GetMessages(Method):
     """
-    Return an array of structs containing details about message
-    templates. If message_ids is specified, only the specified
-    messages will be queried.
+    Returns an array of structs containing details about message
+    templates. If message template_filter is specified and is an array
+    of message template identifiers or hostnames, or a struct of
+    message template attributes, only message templates matching the
+    filter will be returned. If return_fields is specified, only the
+    specified details will be returned.
     """
 
     roles = ['admin']
 
     accepts = [
         Auth(),
-        [Message.fields['message_id']],
+        Mixed([Message.fields['message_id']],
+              Filter(Message.fields)),
+        Parameter([str], "List of fields to return", nullok = True),
         ]
 
     returns = [Message.fields]
 
 
-    def call(self, auth, message_ids = None):
-        return Messages(self.api, message_ids)
+    def call(self, auth, message_filter = None, return_fields = None):
+        return Messages(self.api, message_filter, return_fields)
