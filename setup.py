4fe47ec1a445850b0ea3b4e6876ecfc67f7a900e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2007-01-30 11:37:02 +0000

    removes wildcard for testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2006 The Trustees of Princeton University
 #
-# $Id: setup.py,v 1.5 2007/01/09 16:33:16 mlhuang Exp $
+# $Id: setup.py,v 1.6 2007/01/30 11:27:12 thierry Exp $
 #
 
 from distutils.core import setup
@@ -15,4 +15,6 @@ setup(py_modules = ['ModPython'],
       scripts = ['plcsh', 'Server.py', 'Test.py'],
       data_files = [('', ['planetlab4.sql']),
                     ('php', ['php/plc_api.php']),
-                    ('migrations', ['migrations/*.sql','migrations/README.txt'])])
+#                    ('migrations', ['migrations/*.sql','migrations/README.txt']),
+                    ('migrations', ['migrations/README.txt']),
+                    ])
