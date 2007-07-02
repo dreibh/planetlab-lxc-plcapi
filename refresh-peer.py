2a0ccc04b935e3cdfdb7a92fd6acaac9744169af
Tony Mack <tmack@cs.princeton.edu>
2007-07-02 19:27:43 +0000

    - adding refresh-peer.py caching script to this module

---
+++ refresh-peer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env plcsh
+# $Id: refresh-peer.py 154 2007-03-28 14:15:55Z thierry $
+
+import sys,os,time
+
+def Run (peername):
+    timestring=time.strftime("%Y-%m-%d-%H-%M-%S")
+    print 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',peername
+    print 'RefreshPeer on %s - starting on %s'%(peername,timestring)
+    print 'xxxxxxxxxx'
+    sys.stdout.flush()
+    start=time.time()
+    result=RefreshPeer(peername)
+    finish=time.time()
+
+    print 'Total duration',finish-start
+    print 'xxxxxxxxxx timers:'
+    keys=result.keys()
+    keys.sort()
+    for key in keys:
+        print key,result[key]
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+def RunInLog (peername):
+    logname="/var/log/refresh-peer-%s.log"%(peername)
+    sys.stdout=open(logname,'a')
+    sys.stderr=sys.stdout
+    Run(peername)
+    sys.stderr.close()
+    sys.stdout.close()
+
+if __name__ == "__main__":
+    
+    for peername in sys.argv[1:]:
+        RunInLog (peername)
+
+
