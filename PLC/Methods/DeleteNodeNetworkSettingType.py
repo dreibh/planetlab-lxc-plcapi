b57aa1ac172a15c3dd8beff72733a51ff23d76bd
Daniel Hokka Zakrisson <dhokka@cs.princeton.edu>
2007-12-05 21:29:01 +0000

    Fix typo.

--- PLC/Methods/DeleteNodeNetworkSettingType.py
+++ PLC/Methods/DeleteNodeNetworkSettingType.py
@@ -30,7 +30,7 @@ class DeleteNodeNetworkSettingType(Method):
     def call(self, auth, nodenetwork_setting_type_id_or_name):
         nodenetwork_setting_types = NodeNetworkSettingTypes(self.api, [nodenetwork_setting_type_id_or_name])
         if not nodenetwork_setting_types:
-            raise PLCInvalidArgument, "No such nodenetwork settnig type"
+            raise PLCInvalidArgument, "No such nodenetwork setting type"
         nodenetwork_setting_type = nodenetwork_setting_types[0]
 
         nodenetwork_setting_type.delete()
