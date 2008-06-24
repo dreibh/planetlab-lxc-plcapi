468e2bdfe1be1b610f9db374219e92e47a7c3b8e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-06-24 22:05:57 +0000

    PLC/Legacy needs get packaged

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(py_modules = ['ModPython'],
-      packages = ['PLC', 'PLC/Methods', 'PLC/Methods/system'],
+      packages = ['PLC', 'PLC/Methods', 'PLC/Legacy', 'PLC/Methods/system'],
       scripts = ['plcsh', 'Server.py'],
       data_files = [('', ['planetlab5.sql']),
                     ('php', ['php/plc_api.php']),
