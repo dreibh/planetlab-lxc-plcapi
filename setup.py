31a47cb82e66d1623dcac7e8ae3bb58ac1447c37
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-11-02 02:53:04 +0000

    package migration script for v4 to v5

--- setup.py
+++ setup.py
@@ -14,7 +14,18 @@ from glob import glob
 setup(py_modules = ['ModPython'],
       packages = ['PLC', 'PLC/Methods', 'PLC/Methods/system', 'PLC/Accessors', 'PLC/Legacy'],
       scripts = ['plcsh', 'Server.py'],
-      data_files = [('', ['planetlab5.sql']),
-                    ('php', ['php/plc_api.php']),
-                    ('migrations', ['migrations/README.txt'] + glob('migrations/[0-9][0-9][0-9]*')),
-                    ])
+      data_files = [
+        ('', ['planetlab5.sql']),
+        ('php', ['php/plc_api.php']),
+        ('migrations', 
+         ['migrations/README.txt',
+          'migrations/extract-views.py'] 
+         + glob('migrations/[0-9][0-9][0-9]*')),
+        ('migrations/v4-to-v5', 
+         ['migrations/v4-to-v5/migrate.sh',
+          'migrations/v4-to-v5/migrate.sed',
+          'migrations/v4-to-v5/migrate.sql',
+          'migrations/v4-to-v5/parse-site-nodegroups.py',
+          'migrations/v4-to-v5/site-nodegroups.def'
+          ]),
+        ])
