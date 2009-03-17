a8511c05b713ba5e7cacf869d7c6f782fd91056a
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-03-17 07:03:58 +0000

    rename in packaging

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ setup(py_modules = ['ModPython'],
          ['migrations/README.txt',
           'migrations/extract-views.py'] 
          + glob('migrations/[0-9][0-9][0-9]*')),
-        ('migrations/v4-to-v5', 
-         ['migrations/v4-to-v5/migrate.sh',
-          'migrations/v4-to-v5/migrate.sed',
-          'migrations/v4-to-v5/migrate.sql',
-          'migrations/v4-to-v5/parse-site-nodegroups.py',
-          'migrations/v4-to-v5/site-nodegroups.def'
+        ('migrations/v4.2-to-4.3', 
+         ['migrations/v4.2-to-4.3/migrate.sh',
+          'migrations/v4.2-to-4.3/migrate.sed',
+          'migrations/v4.2-to-4.3/migrate.sql',
+          'migrations/v4.2-to-4.3/parse-site-nodegroups.py',
+          'migrations/v4.2-to-4.3/site-nodegroups.def'
           ]),
         ])
