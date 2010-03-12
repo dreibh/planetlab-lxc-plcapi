2047b9ace34cfa538917cdf4d4c2d7a621d840a4
Barış Metin <Talip-Baris.Metin@sophia.inria.fr>
2010-03-12 10:28:40 +0000

    try fixing cyclic import

--- PLC/Peers.py
+++ PLC/Peers.py
@@ -13,7 +13,7 @@ from PLC.Parameter import Parameter, Mixed
 from PLC.Filter import Filter
 from PLC.Table import Row, Table
 import PLC.Auth
-from PLC.Shell import *
+
 from PLC.Sites import Site, Sites
 from PLC.Persons import Person, Persons
 from PLC.Keys import Key, Keys
@@ -164,12 +164,11 @@ class Peer(Row):
              'peer_node_id': peer_node_id},
             commit = commit)
 
-        # calling UpdateNode with the node's hostname 
-        # will force the 'hrn' tag to be updated with the 
+        # calling UpdateNode with the node's hostname
+        # will force the 'hrn' tag to be updated with the
         # correct root auth
-        shell = Shell() 
-        UpdateNode = self.api.callable('UpdateNode')
-        UpdateNode(shell.auth, node['node_id'], {'hostname': node['hostname']})  
+        from PLC.Methods.UpdateNode import UpdateNode
+        UpdateNode.__call__(UpdateNode(self.api), auth, node['node_id'], {'hostname': node['hostname']})  
 
     def remove_node(self, node, commit = True):
         """
@@ -182,9 +181,8 @@ class Peer(Row):
         # calling UpdateNode with the node's hostname
         # will force the 'hrn' tag to be updated with the
         # correct root auth
-        shell = Shell() 
-        UpdateNode = self.api.callable('UpdateNode')
-        UpdateNode(shell.auth, node['node_id'], {'hostname': node['hostname']})
+        from PLC.Methods.UpdateNode import UpdateNode
+        UpdateNode.__call__(UpdateNode(self.api), auth, node['node_id'], {'hostname': node['hostname']})  
 
     def add_slice(self, slice, peer_slice_id, commit = True):
         """
