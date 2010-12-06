ae247e567468efe3fd5e54488a6fdd32da11fa96
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-12-06 16:43:07 +0100

    updated doc and comments

--- PLC/Accessors/Accessors_site.py
+++ PLC/Accessors/Accessors_site.py
@@ -32,9 +32,18 @@ import sys
 current_module = sys.modules[__name__]
 
 #### example : attach vlan ids on interfaces
-# The third argument expose_in_api is a boolean flag that tells whether this tag may be handled
-#   through the Add/Get/Update methods as a native field
 #
 #define_accessors(current_module, Interface, "Vlan", "vlan",
 #                  "interface/general", "tag for setting VLAN id",
 #                  get_roles=all_roles, set_roles=tech_roles)
+#
+# The optional expose_in_api is a boolean flag that tells whether this tag may be handled
+#   through the Add/Get/Update methods as a native field
+# e.g. 
+#define_accessors(current_module, Node, "Foo", "foo",
+#                  "node/example", "my own description for foo",
+#                  get_roles=all_roles, set_roles=all_roles)
+# will let you do
+# GetNodes ( {'foo':'*bar*'},['hostname','foo'])
+#
+# 
