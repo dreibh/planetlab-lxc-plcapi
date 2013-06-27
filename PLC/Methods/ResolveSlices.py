3b5488ae78737346dee9cf2a26d8c982dd1eb01e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-06-27 15:06:57 +0200

    have ResolveSlices return 'expires' too

--- PLC/Methods/ResolveSlices.py
+++ PLC/Methods/ResolveSlices.py
@@ -7,8 +7,8 @@ from PLC.Slices import Slice, Slices
 class ResolveSlices(Method):
     """
     This method is similar to GetSlices, except that (1) the returned
-    columns are restricted to 'name' and 'slice_id', and (2) it
-    returns expired slices too. This method is designed to help
+    columns are restricted to 'name', 'slice_id' and 'expires', and
+    (2) it returns expired slices too. This method is designed to help
     third-party software solve slice names from their slice_id
     (e.g. PlanetFlow Central). For this reason it is accessible with
     anonymous authentication (among others).
@@ -19,6 +19,7 @@ class ResolveSlices(Method):
     applicable_fields = {
         'slice_id' : Slice.fields['slice_id'],
         'name' : Slice.fields['name'],
+        'expires': Slice.fields['expires'],
         }
 
     accepts = [
