9fd5f37e64cff5b1c9a9f8a8fbfbf4ae7a981c59
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-08-25 14:20:43 +0200

    use standard json module

--- PLC/API.py
+++ PLC/API.py
@@ -13,20 +13,6 @@ import traceback
 import string
 
 import xmlrpclib
-import simplejson
-# use this one
-json=simplejson
-#try:
-#    # Try to use jsonlib before using simpljson. This is a hack to get around
-#    # the fact that the version of simplejson available for f8 is slightly 
-#    # faster than xmlrpc but not as fast as jsonlib. There is no jsonlib 
-#    # package available for f8, so this has to be installed manually and
-#    # is not expected to always be available. Remove this once we move away
-#    # from f8 based MyPLC's         
-#    import jsonlib
-#    json = jsonlib
-#except:
-#    json = simplejson 
 
 # See "2.2 Characters" in the XML specification:
 #
